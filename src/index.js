81ab3e41739e5512902daabe788c5f4715413ca0
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-11-17 10:47:29 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,28 +5,39 @@ dotenv.config({
 });
 
 // there are two ways to connect to the database
-// 1. write the connection code in ./db/index.js and import it here
-import connectDB from "./db/index.js";
-connectDB();
-// 2. ======================or do it here directly=======================
-// import express from "express";
-// const app=express();
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants.js";
-// (async ()=>{
-//     try{
-//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//        app.on("error",(error)=>{
-//            console.error(error);
-//            throw error;
-//        })
+/*
+1. ======================or do it here directly=======================
+import express from "express";
+const app=express();
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+(async ()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+           console.error(error);
+           throw error;
+       })
 
-//        app.listen(process.env.PORT,()=>{
-//               console.log(`Server is running on port ${process.env.PORT}`);
-//          })
-//     }
-//     catch(error){
-//         console.error(error);
-//         throw error;
-//     }
-// })()
+       app.listen(process.env.PORT,()=>{
+              console.log(`Server is running on port ${process.env.PORT}`);
+         })
+    }
+    catch(error){
+        console.error(error);
+        throw error;
+    }
+})()
+*/
+// 2. write the connection code in ./db/index.js and import it here
+import connectDB from "./db/index.js";
+import app from "./app.js";
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Mongo DB not connected !!!!!!!")
+})
